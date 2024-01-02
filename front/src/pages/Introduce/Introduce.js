586808e3b6f54944d4868a1f3c130c835a173f82
davin0130 <davin0130@naver.com>
2024-01-02 16:19:35 +0900

    Modify:Files:Introduce Page List Seperate

--- front/src/pages/Introduce/Introduce.js
+++ front/src/pages/Introduce/Introduce.js
@@ -16,15 +16,37 @@ const Introduce = () => {
         }, [])
     return (
     <div className='intro'>
-        {intro && intro.map((list) => (
-            <div className='content career'>
-                <div className='title'>{list.title}
-                    {list.category}
-                </div>
-                <div className='desc'>{list.description}</div>
-                <div className='date'>{list.date}</div>
+        <div className='main-banner'>
+            <div className='name'>Dabin Jeong | <span className='job'>Product Manager</span></div>
+            <div className='m-title'>Education</div>
+            <div className='list'>
+                학점은행제 컴퓨터공학과 <br />
+                한양여자대학교 스마트IT과 <br />
+                한양여자대학교 미래창업비즈니스과 <br />
             </div>
-        ))}
+        </div>
+        <div className='career'>
+            <div className='category f-pink-50'>Career</div>
+            {intro && intro.filter(list => list.category === "career").map((list) => (
+                <div className='content career'>
+                    <div className='title'>{list.title}
+                    </div>
+                    <div className='desc'>{list.description}</div>
+                    <div className='date f-purple-50'>{list.date}</div>
+                </div>
+            ))}
+        </div>
+        <div className='experience'>
+            <div className='category'>Experience</div>
+            {intro && intro.filter(list => list.category === "experience").map((list) => (
+                <div className='content'>
+                    <div className='title'>{list.title}
+                    </div>
+                    <div className='desc'>{list.description}</div>
+                    <div className='date f-purple-50'>{list.date}</div>
+                </div>
+            ))}
+        </div>
     </div>
     )
 }
